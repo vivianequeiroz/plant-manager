0a5419ac17849bdf0ce1be10290ab62bee565d89
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-09-17 14:41:11 -0300

    template: add base elements that will compose initial app screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+import homeImage from "./assets/watering.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Easily manage your plants</Text>
+      <Image source={homeImage} />
+      <Text>
+        Don't forget to water your plants anymore. We take care to remember you
+        whenever you need.
+      </Text>
+      <TouchableOpacity>
+        <Text>button will come here</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
