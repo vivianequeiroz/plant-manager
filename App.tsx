b61804d3ef6406b925a149de46ab42690ed77901
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-09-17 14:59:47 -0300

    style: add initial homescreen styling

--- App.tsx
+++ App.tsx
@@ -7,18 +7,21 @@ import {
 } from "react-native";
 
 import homeImage from "./assets/watering.png";
+import colors from "./src/styles/colors";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Easily manage your plants</Text>
+      <Text style={styles.title}>
+        Easily {"\n"} manage your {"\n"} plants
+      </Text>
       <Image source={homeImage} />
-      <Text>
+      <Text style={styles.subtitle}>
         Don't forget to water your plants anymore. We take care to remember you
         whenever you need.
       </Text>
-      <TouchableOpacity>
-        <Text>button will come here</Text>
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonContent}> {">"} </Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -27,8 +30,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 32,
+    fontweight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    margin: 38,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  button: {
+    backgroundColor: colors.green,
     justifyContent: "center",
+    alignContent: "center",
+    borderRadius: 16,
+    paddingHorizontal: 16,
+    margin: 20,
+    width: 56,
+    height: 56,
+  },
+  buttonContent: {
+    color: colors.white,
+    fontSize: 24,
   },
 });
