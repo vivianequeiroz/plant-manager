0bd29f19c0bbd61ef3bf02e3a68e22c7dcca4957
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-09-21 20:47:29 -0300

    template: add user identification screen

---
+++ src/screens/userIdentification.tsx
@@ -0,0 +1,66 @@
+import { Feather } from "@expo/vector-icons";
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+export default function UserIdentification() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.form}>
+          <Text style={styles.emoji}>😀</Text>
+          <Text style={styles.title}>How can we {"\n"} call you?</Text>
+          <TextInput style={styles.textInput}></TextInput>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 54,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    textAlign: "center",
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    marginTop: 20,
+  },
+  emoji: {
+    fontSize: 44,
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: "100%",
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: "center",
+  },
+});
