6903c3c7b277dd1161f13da037bb79bf1b69896e
vivianequeiroz <vivianedsqueiroz@gmail.com>
2022-09-25 11:10:39 -0300

    feat: add subtitle to header

--- src/screens/PlantSelect.tsx
+++ src/screens/PlantSelect.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+
+import { Header } from "../components/Headers";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
-import { Button } from "../components/Button";
 
 export function PlantSelect() {
   return (
     <View style={styles.container}>
-      <Text>Select plant</Text>
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>In which environment</Text>
+        <Text style={styles.subtitle}>do you want to place your plant?</Text>
+      </View>
     </View>
   );
 }
@@ -16,9 +22,22 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "space-around",
     backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.text,
+    lineHeight: 20,
   },
 });
